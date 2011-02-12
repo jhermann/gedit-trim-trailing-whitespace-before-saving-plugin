e33c34db5d003d404ea911477c1c8fe669a8ec0b
Daniel Trebbien <dtrebbien@gmail.com>
2011-02-12 14:30:14 -0800

    Update the copyright lines.

--- src/trim-trailing-whitespace-before-saving.py
+++ src/trim-trailing-whitespace-before-saving.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: nil -*-
-# Copyright (C) 2006-2008 Osmo Salomaa
+# Copyright © 2010–2011 Daniel Trebbien
+# Copyright © 2006–2008 Osmo Salomaa
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
