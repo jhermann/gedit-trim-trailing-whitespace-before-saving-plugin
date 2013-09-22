3f81ab620ee7c601d7734f22c01a858fad5c1790
jhermann <jh@web.de>
2013-09-22 14:00:18 +0200

    install script, instead of manual labour

---
+++ install.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+#
+# Install "gedit" plugins to $HOME
+#
+set -e
+BRANCH="master"
+PLUGIN_DIR="$HOME/.local/share/gedit/plugins"
+
+get_github_commit() {
+    local target user repo sha1 pluginsrc
+    target="$1"; shift
+    user="$1"; shift
+    repo="$1"; shift
+    sha1="$1"; shift
+    pluginsrc="$1"; shift
+
+    wget "https://github.com/${user}/${repo}/archive/${sha1}.zip" -O "${workdir}/${sha1}.zip"
+    unzip -j -u "${workdir}/${sha1}.zip" "${repo}-${sha1}/${pluginsrc}/*" -d "$target"
+}
+
+compile_schema() {
+    local schema
+    schema="$1"; shift
+
+    schema_dir="/usr/share/glib-2.0/schemas"
+    test -f "$schema_dir/$schema" || { \
+        echo "Compiling schema $schema..."; \
+        sudo bash -c "umask 0022; cat $PLUGIN_DIR/$schema >$schema_dir/$schema; glib-compile-schemas $schema_dir"; }
+}
+
+workdir=$(dirname "$0")/".cache"
+test -d "$PLUGIN_DIR" || mkdir -p "$PLUGIN_DIR"
+test -d "$workdir" || mkdir "$workdir"
+
+get_github_commit "$PLUGIN_DIR" dtrebbien gedit-trim-trailing-whitespace-before-saving-plugin $BRANCH src
+compile_schema "org.gnome.gedit.plugins.trimtrailingws.gschema.xml"
+
+ls -lrt "$PLUGIN_DIR"/*.plugin
